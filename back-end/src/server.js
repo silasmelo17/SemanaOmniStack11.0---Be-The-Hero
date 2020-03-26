314b5ede7c18fb98564a36293b356cf316817aab
Silas de Melo Araujo <silasmelo1967@gmail.com>
2020-03-26 02:59:34 -0300

    update

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,12 +1,13 @@
 import express from 'express'
-import Router from './routes/index.js'
+import cors from 'cors'
+import router from './routes'
 
 const app = express()
 const port = 3333
 
+app.use( cors() )
 app.use( express.json() )
-
-app.use( Router )
+app.use( router )
 
 app.listen( port, err => {
     if ( err ) throw err

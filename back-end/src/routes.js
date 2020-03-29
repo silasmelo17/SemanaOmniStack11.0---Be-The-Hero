36238fe72f8a2947165e53ba82594b23bbf865c5
Silas de Melo Araujo <silasmelo1967@gmail.com>
2020-03-29 02:04:26 -0300

    update

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -1,21 +1,54 @@
-import express from 'express'
+const express = require('express')
 
-import SessionController from './controllers/SessionController'
-import ProfileController from './controllers/ProfileController' 
-import IncidentController from './controllers/IncidentController'
-import OngController from './controllers/OngController'
+const { celebrate, Segments, Joi } = require('celebrate')
+
+const SessionController = require('./controllers/SessionController')
+const ProfileController = require('./controllers/ProfileController' )
+const IncidentController = require('./controllers/IncidentController')
+const OngController = require('./controllers/OngController')
 
 const router = express.Router()
 
 router.post( '/sessions', SessionController.create )
 
-router.get( '/profile', ProfileController.index )
+router.get( '/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown(),
+}), ProfileController.index )
 
-router.get( '/incidents', IncidentController.index )
-router.post( '/incidents', IncidentController.create )
-router.delete( '/incidents/:id', IncidentController.remove )
+router.get( '/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number()
+    })
+}), IncidentController.index )
+
+router.post( '/incidents', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown(),
+    [Segments.BODY]: Joi.object().keys({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        value: Joi.number().required()
+    })
+}), IncidentController.create )
+
+router.delete( '/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required()
+    })
+}), IncidentController.remove )
 
 router.get( '/ongs', OngController.index )
-router.post( '/ongs', OngController.create )
+router.post( '/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2)
+    })
+}), OngController.create )
 
-export default router
+module.exports = router
--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,13 +1,5 @@
-import express from 'express'
-import cors from 'cors'
-import router from './routes'
-
-const app = express()
+const app = require( './app.js' )
 const port = 3333
-
-app.use( cors() )
-app.use( express.json() )
-app.use( router )
 
 app.listen( port, err => {
     if ( err ) throw err
